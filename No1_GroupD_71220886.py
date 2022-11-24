b7593fda37fab28ba7894934bdf153cb8631907f
jennyrumiris <117902222+jennyrumiris@users.noreply.github.com>
2022-11-24 16:13:53 +0700

    Add files via upload

---
+++ No1_GroupD_71220886.py
@@ -0,0 +1,17 @@
+penjumlahan = 1
+pengurangan = 2
+perkalian = 3
+pembagian = 4
+modulus = 5
+pangkat = 6
+print("ketik 1 untuk menghitung penjumlahan")
+print("ketik 2 untuk menghitung pengurangan")
+print("ketik 3 untuk menghitung penjumlahan")
+print("ketik 4 untuk menghitung penjumlahan")
+print("ketik 5 untuk menghitung penjumlahan")
+print("ketik 6 untuk menghitung penjumlahan")
+anda = (input("masukkan pilihan anda:"))
+p = int(input("masukkan bilangan pertama:"))
+k = int(input("masukkan bilangan kedua:"))
+hasil = p + k
+print("hasil dari", p, "dijumlahkan dengan", k, "adalah", hasil)
